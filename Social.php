b3b5c7603bb6ba616a90909612039eb9aebc5cc1
brianokinyi <brianokinyi.bo@gmail.com>
2018-04-30 23:35:10 +0000

    Minor changes

--- Social.php
+++ Social.php
@@ -8,7 +8,7 @@
 
         public function social()
         {
-            $ussd_text = "Tweet at: @csokimathi\nLike us on Facebook: ComputerSocietyOfKimathi";
+            $ussd_text = "We are Social!\nTweet at: @csokimathi\nLike us on Facebook: ComputerSocietyOfKimathi";
             ussd_proceed($ussd_text);
         }
     }
