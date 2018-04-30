aebadbf3cb5004a2310f0a7e7c0f3aeb584447e7
brianokinyi <brianokinyi.bo@gmail.com>
2018-04-30 23:18:37 +0000

    Added Social class

---
+++ Social.php
@@ -0,0 +1,14 @@
+<?php
+    class Social
+    {
+        public function __construct()
+        {
+            // 
+        }
+
+        public function social()
+        {
+            
+        }
+    }
+?>
