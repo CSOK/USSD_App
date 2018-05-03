9934408fd21f1b86d1641e742f87cbcf36272a6c
brianokinyi <brianokinyi.bo@gmail.com>
2018-05-03 13:50:30 +0000

    sample Settings

---
+++ settings.example.php
@@ -0,0 +1,16 @@
+<?php
+    //LEJA.CO.KE database
+    $servername = "";
+    $dbase_username = "";
+    $password = "";
+    $dbname="";
+
+    // africas Talking
+    $username   = "";
+    $apikey     = "";
+
+    // Registration receiver money
+    $secretary_mobile = "";
+    $secretary_name = "";
+
+?>
